acc772b27901028879a8b1170b2506a0f94dbeb2
Lucas <kunyumaropia@gmail.com>
2026-08-31 00:59:17 +0300

    feat update Epic Fury content

--- Epic Fury/app.js
+++ Epic Fury/app.js
@@ -18,6 +18,13 @@ const addToCart = productBox => {
   const productTitle = productBox.querySelector(".product-title").textContent; 
   const productPrice = productBox.querySelector(".price").textContent;
  
+  const cartItems = cartContent.querySelectorAll(".cart-product-title");
+  for(let item of cartItems){
+    if(item.textContent === productTitle){
+    alert("This items is already in the cart.");
+    return;
+    }
+  }
   const cartBox = document.createElement("div");
   cartBox.classList.add("cart-box");
   cartBox.innerHTML = `<img src="${productImgsrc}" class="cart-img">
@@ -31,6 +38,72 @@ const addToCart = productBox => {
                 </div>
             </div>
            
-           <img src="delete-bin-6-line.png"> `;
+           <img src="delete-bin-6-line.png" class="cart-remove"> `;
            cartContent.appendChild(cartBox);
+    cartBox.querySelector(".cart-remove").addEventListener("click", () =>{
+        cartBox.remove();
+
+         updateCartCount(-1);
+
+        updateTotalPrice();
+    });
+    cartBox.querySelector(".cart-quantity").addEventListener("click", event => {
+        const numberElement = cartBox.querySelector(".number");
+        const decrementButton = cartBox.querySelector("#decrement");
+        let quantity = numberElement.textContent;
+
+        if (event.target.id === "decrement" && quantity > 1){
+            quantity--;
+            if(quantity === 1){
+                decrementButton.style.color = "#999";
+            }
+        } else if(event.target.id === "increment"){
+              quantity++;
+              decrementButton.style.color = "#333";
+        }
+        numberElement.textContent = quantity;
+        updateTotalPrice();
+    });
+    updateCartCount(1);
+    updateTotalPrice();
 };
+const updateTotalPrice = () => {
+    const totalPriceElement = document.querySelector(".total-price");
+    const cartBoxes = cartContent.querySelectorAll(".cart-box");
+    let total = 0;
+    cartBoxes.forEach(cartBox => {
+        const priceElement = cartBox.querySelector(".cart-price");
+        const quantityElement = cartBox.querySelector(".number");
+        const price = priceElement.textContent.replace("ksh", "");
+        const quantity = quantityElement.textContent;
+        total += price * quantity;
+    })
+    totalPriceElement.textContent = `ksh ${total}`;
+};
+
+let cartItemCount = 0;
+const updateCartCount = change => {
+    const cartItemCountBadge = document.querySelector(".cart-item-count");
+    cartItemCount += change;
+    if(cartItemCount > 0) {
+        cartItemCountBadge.style.visibility = "visible";
+        cartItemCountBadge.textContent = cartItemCount;
+    } else{
+        cartItemCountBadge.style.visibility = "hidden";
+        cartItemCountBadge.textContent = "";
+    }
+}
+
+const buyNowButton = document.querySelector(".btn-buy");
+buyNowButton.addEventListener("click", () => {
+    const cartBoxes = cartContent.querySelectorAll(".cart-box");
+    if (cartBoxes.length === 0){
+        alert("Your cart is empty. Please add items to your cart before buying");
+        return;
+    }
+    cartBoxes.forEach(cartBox => cartBox.remove());
+    cartItemCount = 0;
+    updateCartCount  (0);
+    updateTotalPrice();
+    alert("Thank you for your purchase!");
+});
